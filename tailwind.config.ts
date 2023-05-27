8c57b29051a68f787f2c414ef61e7869ff27c6aa
stelko <stilyanmtoskov@gmail.com>
2023-05-27 14:56:04 +0100

    eslint, prettier, and tailwind setup and install

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'soft-blue': 'hsl(215,51%,70%)',
+        cyan: 'hsl(178,100%,50%)',
+        'very-dark-blue-main': 'hsl(217,54%,11%)',
+        'very-dark-blue-card': 'hsl(216,50%,16%)',
+        'very-dark-blue-line': 'hsl(215,32%,27%)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
