a767efab53f94a65244dc4949312f0333c75c695
stelko <stilyanmtoskov@gmail.com>
2023-05-29 21:27:47 +0100

    layout prettier error fix, sharp install

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className='h-full'>
+    <html lang="en" className="h-full">
       <body className={`${outfit.className} h-full`}>{children}</body>
     </html>
   );
