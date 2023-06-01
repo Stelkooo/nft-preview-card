c8cefe93f4977c4dd9c618f52dca9836745f61ef
stelko <stilyanmtoskov@gmail.com>
2023-06-01 19:09:45 +0100

    desktop gaps amended

--- components/card/card.component.tsx
+++ components/card/card.component.tsx
@@ -5,10 +5,10 @@ import Figure from '../figure/figure.component';
 
 export default function Card() {
   return (
-    <article className="flex max-w-xs flex-col gap-6 rounded-2xl bg-very-dark-blue-card p-6 lg:max-w-[350px]">
+    <article className="flex max-w-xs flex-col gap-6 rounded-2xl bg-very-dark-blue-card p-6 lg:max-w-[350px] lg:pb-8">
       <Figure />
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:gap-6">
+        <div className="flex flex-col gap-4 lg:gap-6">
           <Description />
           <CostAndTime />
         </div>
--- components/description/description.component.tsx
+++ components/description/description.component.tsx
@@ -1,6 +1,6 @@
 export default function Description() {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3 lg:gap-4">
       <h1 className="max-w-fit text-xl font-semibold text-white transition-colors hover:cursor-pointer hover:text-cyan">
         Equilibrium #3429
       </h1>
